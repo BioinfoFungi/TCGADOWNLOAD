70c4a0d0a0880f8b6d6771d846491d23a4cdc122
WangYang <1749748955@qq.com>
2021-08-25 01:58:29 +0800

    update

---
+++ TCGA_DOENLOAD_MiRNA_COUNT.R
@@ -0,0 +1,29 @@
+library(TCGAbiolinks)
+getwd()
+#####下载COUNT
+if(!file.exists("GDCdata/Counts")){
+    dir.create("GDCdata/Counts",recursive = T)
+}
+if(!file.exists("result")){
+    dir.create("result")
+}
+
+project <- paste0("TCGA-",cancer)
+message(project)
+match.file.cases.all <- NULL
+filename <- gsub("-","_",paste0("result/",project,"_miRNA_COUNTS.tsv"))
+query<- GDCquery(project = project,
+        data.category = "Transcriptome Profiling",
+        data.type = "miRNA Expression Quantification",
+        workflow.type = "BCGSC miRNA Profiling")
+
+GDCdownload(query,directory = "GDCdata/miRNA",method = "api")
+data <- GDCprepare(query,
+        directory = "GDCdata/miRNA",
+        summarizedExperiment = F,
+        save = F)
+readr::write_tsv(data,file =filename )
+system(paste0("gzip ",filename))
+
+cat("$update:true","\n")
+cat("$absolutePath:",paste0(workDir,"/",filename,".gz"))
