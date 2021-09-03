5a638335bcac1c80907fe6e2344bdd43bdb2ff90
wangyang1749 <1749748955@qq.com>
2021-09-03 11:57:53 +0800

    update

---
+++ R/GPL_Process.R
@@ -0,0 +1,29 @@
+library(GEOquery)
+library(tidyverse)
+
+if(!exists(absolutePath)){
+    absolutePath <- "/home/wangyang/Public/bioinfo/BRCA/data/GPL19978.soft.gz"
+    probeId <- "ID"
+    symbol<- "circRNA"
+    workDir <- "."
+    enName <- "GPL19978"
+}
+probeId_ <- probeId
+symbol_ <- symbol
+cat(absolutePath,"\n")
+gpl_obj <- getGEO(filename = absolutePath)
+gpl <- gpl_obj@dataTable@table |> 
+    dplyr::select("ID"=probeId,symbol=symbol_) -> gpl_two
+
+
+saveFile <- paste0(workDir,"/GPL/",enName,".tsv")
+if(!file.exists("GPL")){
+     dir.create("GPL",recursive = T)
+}
+
+write_tsv(gpl_two, file=saveFile)
+system(paste0("gzip -f ",saveFile))
+# cat(symbol,"\n")
+# cat(probeId,"\n")
+cat("$absolutePath:",paste0(saveFile,".gz"),"\n")
+
